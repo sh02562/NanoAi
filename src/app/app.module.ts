e2741b90c354db7213a99022e6d951d8dbb71848
aknigam@ahex.co.in <aknigam@ahex.co.in>
2020-09-15 18:04:07 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignInComponent } from './Login/sign-in/sign-in.component';
+import { SignUpComponent } from './Login/sign-up/sign-up.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignInComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
